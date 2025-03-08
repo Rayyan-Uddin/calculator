404ed7122e79cd31f9c099ae684624cf65d7bbb4
Rayyan Uddin <rayyanuddin007@gmail.com>
2025-03-08 16:27:01 +0500

    UI

---
+++ script.js
@@ -0,0 +1,42 @@
+let num1;
+let operator;
+let num2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ADDITION
+function add(a, b) {
+    return a + b;
+}
+//SUBTRACTION
+function subtract(a, b) {
+    return a - b;
+}
+//MULTIPLICATION
+function multiply(a, b) {
+    return a * b;
+}
+//DIVISION
+function divide(a, b) {
+    return a / b;
+}
+
+//OPERATION
+function operate(num1, num2, operator) {
+
+}
